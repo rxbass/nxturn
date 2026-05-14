41b2f018a7a7cf53997c75be396db3f437ead490
Jo Tag <jothibasus@techaffinity.com>
2026-05-14 17:40:06 +0530

    Serve marketing.html at root / without redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
-import { useEffect } from 'react'
 import ReceptionistPage from './pages/ReceptionistPage'
 import PatientStatusPage from './pages/PatientStatusPage'
 import DisplayPage from './pages/DisplayPage'
 import DoctorPage from './pages/DoctorPage'
 
-function Home() {
-  useEffect(() => { window.location.replace('/marketing.html') }, [])
-  return null
-}
-
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
       <Route path="/receptionist/:clinicId" element={<ReceptionistPage />} />
       <Route path="/receptionist" element={<ReceptionistPage />} />
       <Route path="/status/:token" element={<PatientStatusPage />} />
